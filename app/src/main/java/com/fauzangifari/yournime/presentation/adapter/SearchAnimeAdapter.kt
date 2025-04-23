dea9d5e16bd117832c8152aca768bc348139ef1d
Fauzan Gifari <fauzan.gifari30@gmail.com>
2025-04-23 22:02:47 +0800

    feat(search): Enhance search layout with RecyclerView for results display

---
+++ app/src/main/java/com/fauzangifari/yournime/presentation/adapter/SearchAnimeAdapter.kt
@@ -0,0 +1,62 @@
+package com.fauzangifari.yournime.presentation.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.fauzangifari.core.domain.model.Anime
+import com.fauzangifari.yournime.R
+import org.w3c.dom.Text
+
+class SearchAnimeAdapter(
+    private val onItemClick: (Anime) -> Unit
+) : ListAdapter<Anime, SearchAnimeAdapter.AnimeViewHolder>(AnimeDiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AnimeViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_list_anime_detail, parent, false)
+        return AnimeViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: AnimeViewHolder, position: Int) {
+        val anime = getItem(position)
+        holder.bind(anime)
+    }
+
+    inner class AnimeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val imgAnime: ImageView = itemView.findViewById(R.id.imgAnime)
+        private val tvTitle: TextView = itemView.findViewById(R.id.tvTitle)
+        private val tvScore: TextView = itemView.findViewById(R.id.tvScore)
+        private val tvInfo: TextView = itemView.findViewById(R.id.tvInfo)
+        private val tvSeason: TextView = itemView.findViewById(R.id.tvSeason)
+        private val tvGenre: TextView = itemView.findViewById(R.id.tvGenre)
+
+        fun bind(anime: Anime) {
+            tvTitle.text = anime.title
+            Glide.with(itemView.context)
+                .load(anime.images)
+                .into(imgAnime)
+            tvScore.text = "⭐ ${anime.score} • ${anime.scoredBy} votes"
+            tvInfo.text = "${anime.type} • ${anime.episodes} episodes"
+            tvSeason.text = anime.season
+            tvGenre.text = anime.genre.joinToString(", ") { it.name }
+
+            itemView.setOnClickListener {
+                onItemClick(anime)
+            }
+        }
+    }
+
+    class AnimeDiffCallback : DiffUtil.ItemCallback<Anime>() {
+        override fun areItemsTheSame(oldItem: Anime, newItem: Anime): Boolean =
+            oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: Anime, newItem: Anime): Boolean =
+            oldItem == newItem
+    }
+}
